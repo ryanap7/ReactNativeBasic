a0dfd537d04fd02c196f90417c2652ad7ff2d9cc
Ryan1604 <Ryan.Aprianto77777@gmail.com>
2020-11-11 15:48:45 +0700

    Edit App.js

--- App.js
+++ App.js
@@ -40,25 +40,6 @@ const App: () => React$Node = () => {
           )}
           <View style={styles.body}>
             <Text>Hello World</Text>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
           </View>
         </ScrollView>
       </SafeAreaView>
